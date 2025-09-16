cf83630e20c942f24f9675cb9b38379c3a494549
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-16 14:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function helper(){
+    let text=document.getElementById("text").value
+    let delay=parseInt(document.getElementById("delay").value)
+	return new Promise((resolve,reject)=>{
+		setTimeout(() => {
+            resolve(text)
+        }, delay);
+	})
+}
+async function run(event){
+    event.preventDefault();
+    let text=await helper()
+    console.log(text)
+    document.getElementById("output").innerText=text
+}
